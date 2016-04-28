4390c3d8da72d89fa4f9f114bb0b3d2260432076
Daniel Carr <d.douglas.carr@gmail.com>
2016-04-28 19:46:15 +1000

    DelegationsController.getOutgoingDelegations()

---
+++ src/main/java/org/ddouglascarr/controllers/DelegationsController.java
@@ -0,0 +1,42 @@
+package org.ddouglascarr.controllers;
+
+import org.ddouglascarr.aop.HandleServiceErrors;
+import org.ddouglascarr.exceptions.ItemNotFoundException;
+import org.ddouglascarr.exceptions.MemberUnprivilegedException;
+import org.ddouglascarr.models.Delegation;
+import org.ddouglascarr.models.UserDetailsImpl;
+import org.ddouglascarr.services.DelegationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/units/{unitId}/members/{memberId}/delegations")
+public class DelegationsController
+{
+    @Autowired
+    private DelegationService delegationService;
+
+    @HandleServiceErrors
+    @RequestMapping(
+            value = "/",
+            method = RequestMethod.GET
+    )
+    public ResponseEntity<List<Delegation>> getOutgoingDelegations(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @PathVariable Long unitId,
+            @PathVariable Long memberId)
+            throws ItemNotFoundException, MemberUnprivilegedException
+    {
+        List<Delegation> delegations = delegationService
+                .findByTrusterId(userDetails.getId(), unitId, memberId);
+        return new ResponseEntity<>(delegations, HttpStatus.OK);
+    }
+}
---
+++ src/test/java/org/ddouglascarr/unit/DelegationsControllerTests.java
@@ -0,0 +1,64 @@
+package org.ddouglascarr.unit;
+
+import org.ddouglascarr.controllers.DelegationsController;
+import org.ddouglascarr.models.Delegation;
+import org.ddouglascarr.models.UserDetailsImpl;
+import org.ddouglascarr.services.DelegationService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+public class DelegationsControllerTests
+{
+    @Mock
+    private DelegationService delegationService;
+
+    @InjectMocks
+    private DelegationsController delegationsController;
+
+    // NOTE: If a user is looking up another member's delegation, their id is USER_DETAILS_ID
+    private static Long MEMBER_ID = new Long(1);
+    private static Long USER_DETAILS_ID = new Long(6);
+    private static Long UNIT_ID = new Long(2);
+    private static Long DELEGATION_ID = new Long(3);
+    private static Long TRUSTER_ID = new Long(4);
+    private static Long TRUSTEE_ID = new Long(5);
+
+    @Mock
+    Delegation mockDelegation;
+    @Mock
+    UserDetailsImpl userDetails;
+
+    List<Delegation> mockDelegationList;
+
+    @Before
+    public void setup()
+    {
+        MockitoAnnotations.initMocks(this);
+        when(userDetails.getId()).thenReturn(USER_DETAILS_ID);
+        when(mockDelegation.getId()).thenReturn(DELEGATION_ID);
+        mockDelegationList = new ArrayList<Delegation>();
+    }
+
+    @Test
+    public void getOutgoingDelegationsReturnsTrusterDelegationsForMember() throws Exception
+    {
+        when(delegationService.findByTrusterId(USER_DETAILS_ID, UNIT_ID, MEMBER_ID))
+                .thenReturn(mockDelegationList);
+        ResponseEntity<List<Delegation>> resp = delegationsController
+                .getOutgoingDelegations(userDetails, UNIT_ID, MEMBER_ID);
+        assertEquals(HttpStatus.OK, resp.getStatusCode());
+        List<Delegation> returnedList = resp.getBody();
+        assertEquals(mockDelegationList, returnedList);
+    }
+}
