cd4fbb5373ca4a3b9da2a2bdef9f59a0f55fcba9
Daniel Carr <d.douglas.carr@gmail.com>
2016-05-05 14:32:24 +1000

    add unit_id to area_delegation view

--- src/test/resources/sql/liquid-canon.sql
+++ src/test/resources/sql/liquid-canon.sql
@@ -7,3 +7,18 @@ CREATE TABLE "unit_permissions" (
         "public_read"   BOOLEAN         NOT NULL);
 COMMENT ON COLUMN "unit_permissions"."public_read" IS 'Set to TRUE if only members can read data from unit';
 
+DROP VIEW area_delegation;
+CREATE OR REPLACE VIEW area_delegation AS
+ SELECT DISTINCT ON (area.id, delegation.truster_id) area.id AS area_id,
+    area.unit_id,
+    delegation.id,
+    delegation.truster_id,
+    delegation.trustee_id,
+    delegation.scope
+   FROM area
+     JOIN delegation ON delegation.unit_id = area.unit_id OR delegation.area_id = area.id
+     JOIN member ON delegation.truster_id = member.id
+     JOIN privilege ON area.unit_id = privilege.unit_id AND delegation.truster_id = privilege.member_id
+  WHERE member.active AND privilege.voting_right
+  ORDER BY area.id, delegation.truster_id, delegation.scope DESC;
+
