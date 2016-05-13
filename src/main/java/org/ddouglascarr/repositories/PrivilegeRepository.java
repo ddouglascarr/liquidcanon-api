c9bd8d59f4b009c507b3ea3cd4905d4baa475164
Daniel Carr <d.douglas.carr@gmail.com>
2016-05-13 16:37:36 +1000

    de-hibernate PrivilegeRepository

--- src/main/java/org/ddouglascarr/repositories/PrivilegeRepository.java
+++ src/main/java/org/ddouglascarr/repositories/PrivilegeRepository.java
@@ -4,8 +4,7 @@ import org.ddouglascarr.models.Privilege;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface PrivilegeRepository extends JpaRepository<Privilege, Privilege.PrivilegeId>
+public interface PrivilegeRepository
 {
     Privilege findOneByMemberIdAndUnitId(Long memberId, Long unitId);
 }
---
+++ src/main/java/org/ddouglascarr/repositories/PrivilegeRepositoryImpl.java
@@ -0,0 +1,36 @@
+package org.ddouglascarr.repositories;
+
+
+import org.ddouglascarr.models.Privilege;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PrivilegeRepositoryImpl implements PrivilegeRepository
+{
+    @Autowired
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @Override
+    public Privilege findOneByMemberIdAndUnitId(Long memberId, Long unitId)
+    {
+        String sql = String.join(" ",
+                "SELECT * FROM privilege",
+                "WHERE member_id = :memberId AND unit_id = :unitId");
+        MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+        namedParameters.addValue("memberId", memberId);
+        namedParameters.addValue("unitId", unitId);
+        try {
+            Privilege privilege = (Privilege) namedParameterJdbcTemplate.queryForObject(
+                    sql, namedParameters, new BeanPropertyRowMapper<Privilege>(Privilege.class));
+            return privilege;
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+}
