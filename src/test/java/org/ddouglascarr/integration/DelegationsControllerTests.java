d97a3cad8ac66c1a6fb5331077d937662da6853c
Daniel Carr <d.douglas.carr@gmail.com>
2016-05-15 16:53:12 +1000

    successful http query

--- src/test/java/org/ddouglascarr/integration/DelegationsControllerTests.java
+++ src/test/java/org/ddouglascarr/integration/DelegationsControllerTests.java
@@ -1,6 +1,7 @@
 package org.ddouglascarr.integration;
 
 import org.ddouglascarr.LiquidcanonApplication;
+import org.ddouglascarr.config.WebSecurityConfig;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,17 +18,21 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
+
+
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration(classes = {LiquidcanonApplication.class})
+@ContextConfiguration(classes = {LiquidcanonApplication.class, WebSecurityConfig.class})
 @TestPropertySource(locations = "classpath:test.properties")
 @Transactional
 @Sql(
         executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
         scripts = {
-                // "classpath:sql/liquid-canon.sql",
+                "classpath:sql/liquid-canon.sql",
                 "classpath:sql/test-world.sql"
         }
 )
@@ -42,13 +47,16 @@ public class DelegationsControllerTests
     @Before
     public void setup() throws Exception
     {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .build();
     }
 
     @Test
     public void testTest() throws Exception
     {
-        mockMvc.perform(get("/units/1"))
+        mockMvc.perform(get("/units/1").with(httpBasic("tender_hugle", "login")))
                 .andExpect(status().isOk());
     }
 
