64441d83feb02c9ce7d3ab97e066c825b37036d1
Daniel Carr <d.douglas.carr@gmail.com>
2016-05-16 22:17:01 +1000

    disable RepositoryRestHandlerMapping

--- src/main/java/org/ddouglascarr/LiquidcanonApplication.java
+++ src/main/java/org/ddouglascarr/LiquidcanonApplication.java
@@ -3,9 +3,10 @@ package org.ddouglascarr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
 @EnableTransactionManagement
 public class LiquidcanonApplication
 {
