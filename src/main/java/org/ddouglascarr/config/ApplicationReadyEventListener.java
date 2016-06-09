2322ca20cf9a7e46ab1833030b174418fc962d89
Daniel Carr <d.douglas.carr@gmail.com>
2016-06-09 12:08:48 +1000

    CreateAdminMember command fired on startup w/out admins

---
+++ src/main/java/org/ddouglascarr/config/ApplicationReadyEventListener.java
@@ -0,0 +1,32 @@
+package org.ddouglascarr.config;
+
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.ddouglascarr.command.member.commands.CreateAdminMemberCommand;
+import org.ddouglascarr.query.services.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class ApplicationReadyEventListener implements ApplicationListener<ApplicationReadyEvent>
+{
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private CommandGateway commandGateway;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent)
+    {
+        System.out.println("applicationReadyEvent");
+        if (!memberService.isAtLeastOneAdminMember()) {
+            CreateAdminMemberCommand command = new CreateAdminMemberCommand(
+                    UUID.randomUUID(), "admin", "password" );
+            commandGateway.send(command);
+        }
+    }
+}
